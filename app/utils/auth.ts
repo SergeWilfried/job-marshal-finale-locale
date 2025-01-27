45daf25416bd358be344072607acc7a404b4f037
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-01-27 16:05:04 +0000

    Update auth.ts

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -6,5 +6,15 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub, Google],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
