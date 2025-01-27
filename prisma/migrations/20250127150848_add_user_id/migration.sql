bfe2920cb18d6a1af661e3d2c9de4831b5d288a7
SergeWilfried <willytheigeek@gmail.com>
2025-01-27 15:11:34 +0000

    fix: db migrations

---
+++ prisma/migrations/20250127150848_add_user_id/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userId]` on the table `JobPost` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userId` to the `JobPost` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "JobPost" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "JobPost_userId_key" ON "JobPost"("userId");
